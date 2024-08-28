96fc0d96928562d6f4150a6a44473d5d13949626
vagnertomas <vagnertomasv7@gmail.com>
2024-08-27 22:14:29 -0300

    trabalho

--- Trabalho.c
+++ Trabalho.c
@@ -4,10 +4,13 @@ double soma(double a, double b);
 double subtracao(double a, double b);
 double multiplicacao(double a, double b);
 double divisao(double a, double b);
+double potenciacao(double ba, int exp);
+
 int main() {
 
     int escolha;
-    double A = 0, B = 0, R = 0;
+    double A = 0, B = 0, R = 0.0;
+	int E = 0;
 
     printf("Escolha uma operacao:\n");
     printf("0. Sair\n");
@@ -39,11 +42,18 @@ int main() {
         scanf("%lf\n%lf", &A, &B);
         R = multiplicacao(A,B);
         printf("o valor da multiplicacao e %.5lf",R);
-    }  else if (escolha == 4) {
+    } else if (escolha == 4) {
         printf("informe os numeros que deseja dvidir\n");
         scanf("%lf\n%lf", &A, &B);
         R = divisao(A,B);
         printf("o valor da divisao e %.5lf",R);
+    } else if (escolha == 5) {
+        printf("informe a base\n");
+        scanf("%lf", &A);
+        printf("informe o expoente\n");
+        scanf("%d", &E);
+        R = potenciacao(A,E);
+        printf("o valor da potenciacao e %.5lf",R);
     }
 
     return 0;
@@ -77,3 +87,13 @@ double divisao(double a, double b){
     return(R);
 }
 
+double potenciacao(double ba, int exp){
+    double N = 1.0;
+
+    for(int i = 0; i < exp; i++)
+    {
+        N = N*ba;
+    }
+
+    return(N);
+}
